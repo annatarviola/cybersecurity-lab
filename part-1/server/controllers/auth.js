fc03776fa0745f6a85fb7850bc50997972d32dbd
Lily Springer <lilyspringer@hotmail.com>
2023-03-04 17:22:42 -0700

    code complete

--- part-1/server/controllers/auth.js
+++ part-1/server/controllers/auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs')
 const users = []
 
 module.exports = {
@@ -6,6 +7,14 @@ module.exports = {
       console.log(req.body)
       const { username, password } = req.body
       for (let i = 0; i < users.length; i++) {
+        if (users[i].username === username) {
+          const authenticated = bcrypt.compareSync([password, users[i].pwordHash])
+          if (authenticated) {
+            let userToReturn = {...users[i]}
+            delete userToReturn.pwordHash
+            res.status(200).send(userToReturn)
+          }
+        }
         if (users[i].username === username && users[i].password === password) {
           res.status(200).send(users[i])
         }
@@ -17,5 +26,18 @@ module.exports = {
         console.log(req.body)
         users.push(req.body)
         res.status(200).send(req.body)
+        const { username, email, firstName, lastName, password } = req.body
+        const salt = bcrypt.getSaltSync(5)
+        const pwordHash = bcrypt.hashSync(password, salt)
+        const user = {
+          username,
+          email,
+          firstName,
+          lastName,
+          pwordHash
+        }
+        users.push(user)
+        let userToReturn = {...user}
+        delete userToReturn.pwordHash
     }
 }
